b5bac71be04310c98ea0c21f35fb24c2ebcb19c5
stareginio <sophiecodes02@gmail.com>
2022-11-21 23:38:31 +0800

    added navigation buttons in MainActivity for settings, add, and edit pages. added AddRecordActivity.

--- app/src/main/java/com/reginio/expensetracker/MainActivity.java
+++ app/src/main/java/com/reginio/expensetracker/MainActivity.java
@@ -1,14 +1,50 @@
 package com.reginio.expensetracker;
 
+import android.content.*;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button settingsBtn, addBtn, editBtn;
+    Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        settingsBtn = (Button) findViewById(R.id.btnSettings);
+        addBtn = (Button) findViewById(R.id.btnAdd);
+        editBtn = (Button) findViewById(R.id.btnEdit);
+
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nextActivity(SettingsActivity.class);
+            }
+        });
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nextActivity(AddRecordActivity.class);
+            }
+        });
+
+//        editBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                nextActivity(EditRecordActivity.class);
+//            }
+//        });
+    }
+
+    private void nextActivity(Class dest) {
+        intent = new Intent(MainActivity.this, dest);
+        startActivity(intent);
     }
 }
