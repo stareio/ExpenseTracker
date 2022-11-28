7813dc77e2e25c02c3f1f2566ff125a68d168654
maikerucode <michaeljoshuasarmiento@gmail.com>
2022-11-28 20:40:50 +0800

    functional redirect to addRecord from checkRecord

--- app/src/main/java/com/reginio/expensetracker/CheckRecordActivity.java
+++ app/src/main/java/com/reginio/expensetracker/CheckRecordActivity.java
@@ -2,13 +2,28 @@ package com.reginio.expensetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class CheckRecordActivity extends AppCompatActivity {
+
+    ImageButton addBtn;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_record);
+
+        addBtn = (ImageButton) findViewById(R.id.btnAdd);
+
+        addBtn.setOnClickListener(view -> nextActivity(AddRecordActivity.class));
+    }
+
+    private void nextActivity(Class dest) {
+        intent = new Intent(CheckRecordActivity.this, dest);
+        startActivity(intent);
     }
 }
