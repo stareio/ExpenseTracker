4d3c35b05834f22cfff71217f6fb2a670ebdcc9a
stareginio <sophiecodes02@gmail.com>
2022-12-03 07:19:27 +0800

    updated retrieval of list of records in MainActivity.java. added custom adapter RecordAdapter.java and interface OnEditRecordSpnrSelect.java for retrieving list of records. added retrieval of "id" in DBHandler.java. added spinner in list_record.xml.

---
+++ app/src/main/java/com/reginio/expensetracker/OnEditRecordSpnrSelect.java
@@ -0,0 +1,10 @@
+package com.reginio.expensetracker;
+
+public interface OnEditRecordSpnrSelect {
+    void onItemSelectedListener(String modify, int id);
+}
+/*
+Reference
+Send string from custom Adapter to Activity (1): https://stackoverflow.com/questions/48048977/send-string-from-custom-adapter-to-activity
+Send string from custom Adapter to Activity (2): https://stackoverflow.com/questions/35008860/how-to-pass-values-from-recycleadapter-to-mainactivity-or-other-activities
+*/
